2b1b71c55f1caf9d292ab68de0dd48110c39c713
RedRedRyan <ryankipkorir2002@gmail.com>
2025-04-17 13:57:04 +0300

    UI

--- Distance/DistanceCalc.cs
+++ Distance/DistanceCalc.cs
@@ -4,7 +4,9 @@ using UnityEngine.UI;
 public class DistanceCalc : MonoBehaviour
 {
     public Transform startCube; 
-    public Transform endCube;   
+    public Transform endCube;
+
+    public Image bgroundImage;  
     public Text distanceText;
     private LineRenderer line;    
 
@@ -17,13 +19,16 @@ public class DistanceCalc : MonoBehaviour
 }
     void Update()
     {
-        // Calculate Euclidean distance between the cubes
+        // Calculating The Euclidean distance between the cubes
         float distance = Vector3.Distance(startCube.position, endCube.position);
 
         // Update the UI Text to display the distance
         distanceText.text = "Distance: " + distance.ToString("F2") + " units";
         line.SetPosition(0, startCube.position);
         line.SetPosition(1, endCube.position);
+
+        
+        // bgroundImage(setActive: true);
     }
     
 
